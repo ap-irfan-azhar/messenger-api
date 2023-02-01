4f53727f03238c0efdffd335de8712e8db48c192
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2023-02-01 18:50:28 +0700

    creating end points for login and sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   # encrypt password
   has_secure_password
+  has_many :conversation_users
+  has_many :conversations, through: :conversation_users
+  has_many :messages
+
+  validates_presence_of :email, :name
+  validates_uniqueness_of :email
+
+  def new_attribute
+    {
+      id: self.id,
+      name: self.name
+    }
+  end
 end
