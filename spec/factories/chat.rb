e71b976415382d98d0d8c7e29cf35374c2c81881
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2023-02-02 21:58:49 +0700

    renaming message factory to chat and renaming user_id to sender_id

--- spec/factories/chat.rb
+++ spec/factories/chat.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
-  factory :message do
-    user_id { 0 }
+  factory :chat do
+    sender_id { 0 }
     conversation_id { 0 }
     message {""}
 
     trait :with_attrs do
       conversation_id { |attrs| attrs[:conversation_id]}
-      user_id {|attrs| attrs[:user_id]}
+      sender_id {|attrs| attrs[:user_id]}
       message {|attrs| attrs[:message]}
     end
   end
