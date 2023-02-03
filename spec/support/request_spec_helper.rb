2b4b074ee16e77e7f9770deccb0658f56725ec01
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2023-02-03 14:02:43 +0700

    moving be_json_type matcher to dedicated file

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -1,3 +1,5 @@
+
+require 'be_json_type_matcher'
 module RequestSpecHelper
   # Parse JSON response to ruby hash
   def response_body
@@ -16,28 +18,6 @@ module RequestSpecHelper
       raise e
     end
     expect(response_body).to be_json_type(json) if json
-  end
-end
-
-RSpec::Matchers.define :be_json_type do |expected|
-  match do |actual|
-    match_json_structure_helper(expected, actual)
-  end
-
-  def match_json_structure_helper(expected, actual)
-    expected.each do |key, value|
-      if value.is_a?(Hash)
-        return false unless actual.is_a?(Hash) && actual.key?(key)
-        return false unless match_json_structure_helper(value, actual[key])
-      elsif value == []
-        return false unless actual[key] == []
-      elsif value.kind_of?(Array)
-        return false unless match_json_structure_helper(value.first, actual[key].first)
-      else
-        return false unless actual.is_a?(Hash) && actual.key?(key) && (actual[key] == value ||actual[key].is_a?(value))
-      end
-    end
-    true
   end
 end
 
